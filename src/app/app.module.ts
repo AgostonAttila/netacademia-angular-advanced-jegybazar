a0880e525961c58e9df872043e3b15a70617d338
atesz014 <atesz014@gmail.com>
2018-02-16 19:06:56 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AlertModule, CollapseModule } from 'ngx-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { JumbotronComponent } from './core/jumbotron/jumbotron.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { EventcardComponent } from './event/eventcard/eventcard.component';
+import { AuthInterceptor } from './shared/auth-interceptor';
+import { EventService } from './shared/event.service';
+import { LoggedInGuardGuard } from './shared/logged-in-guard.guard';
+import { TicketService } from './shared/ticket.service';
+import { UserService } from './shared/user.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    JumbotronComponent,
+    EventcardComponent,
+    FooterComponent,
+    ...AppRoutingModule.routableComponents
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CollapseModule.forRoot(),
+    AlertModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    EventService,
+    UserService,
+    TicketService,
+    LoggedInGuardGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
